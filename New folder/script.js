3143df3ba1c6a66f40ba661c9f3dc46121c81006
Lawrence Cardoza <imalawrencecardoza@gmail.com>
2026-06-21 08:13:34 +0530

    ternary

---
+++ New folder/script.js
@@ -0,0 +1,47 @@
+// let js = 'amazing'
+// if (js === 'amazing');
+// // alert('hey')
+
+// let num = 20 + 10 - 20 - 5
+// console.log(num)
+
+// //----------------------
+
+// var name = "Lawrence";
+// name = "Cardoza";
+
+// console.log(name)// Cardoza
+
+// // ------------------------
+
+// const age = 22;
+// age =  32;
+
+// console.log(age)// error
+
+// Standard if-else
+
+// let login = true;
+
+// if(login === true){
+//   console.log("login");
+// }else{
+//   console.log("please login in ")
+// }
+
+let user = ''
+
+// if(user.length === 0){
+//   console.log("Enter your password");
+// } else if (user.length >= 8){
+//   console.log("Strong")
+// }else{
+//   console.log("weak")
+// }
+
+//Ternary
+
+const message = user.length === 0 ? 'Enter your password' : user.length >= 8 ? 'Strong' : 'Weak' // The final default "else"
+
+console.log(message)
+
