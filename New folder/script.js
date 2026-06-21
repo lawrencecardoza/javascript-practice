44404367059ec7333a65b388f8c65224660c1549
Lawrence Cardoza <imalawrencecardoza@gmail.com>
2026-06-21 11:27:21 +0530

    loop

--- New folder/script.js
+++ New folder/script.js
@@ -45,5 +45,75 @@ let user = ''
 
 // console.log(message)
 
-let prices = [10, 25, 5, 40, 15]
+// // for(let i=0; i<prices.length; i++){
+// //   console.log(prices[i])
+// // }
 
+// let prices = [10, 25, 5, 40, 15]
+
+// for (let i = 0; i < prices.length; i++) {
+//   // Check if the current price is greater than or equal to 20
+//   if (prices[i] >= 20) {
+//     console.log(prices[i])
+//   }
+// }
+
+// let numbers = [1, 4, 7, 12, 19, 20, 23];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 === 0) {
+//     console.log(numbers[i])
+//   }
+// }
+
+// let count = 0;
+
+// let transactions = [50, -10, 100, -25, 30, -5];
+
+// for(let i=0; i< transactions.length; i++){
+//   if(transactions[i] <= 0){
+//     count++;
+//     console.log(transactions[i])
+//   }
+// }
+// console.log(`count no of ${count}`)
+
+// -------------------------------
+
+// let count = 0
+
+// for(let i=1; i<=30; i++){
+//   if( i % 3 === 0){
+//     count++
+//     console.log(`Multple of 3 * ${count} = ${i}`)
+//   }
+// }
+
+// let names = ['Alex', 'Lawrence', 'Sam', 'Christina', 'Jo', 'Oliver']
+
+// for (let i = 0; i < names.length; i++) {
+//   if (names[i].length > 5) {
+//     console.log(names[i])
+//   }
+// }
+
+// let inputs = ["Valid Data", "", "More Data", "", "Final Data"];
+// let cleanInputs = []; // Your container to store valid strings
+
+
+// for(let i=0; i < inputs.length; i++){
+//   if(inputs[i] !== ""){
+//     cleanInputs.push(inputs[i])
+//   }
+// }
+// console.log(cleanInputs)
+
+let scores = [45, 80, 95, 100, 60, 75]
+
+for(let i=0; i<scores.length; i++){
+  if(scores[i] === 100){
+    
+    console.log("Target Found!",scores[i]);
+    break;
+  }
+}
