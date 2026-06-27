4ee3e7295ef0fd0ec4bc934665d333ba43a8c7b6
Lawrence Cardoza <imalawrencecardoza@gmail.com>
2026-06-27 08:58:46 +0530

    Solve: sum problem

---
+++ Problem_Solveing/script.js
@@ -0,0 +1,18 @@
+// 1. Print Hello World
+
+console.log('Hello World');
+
+// 2. Even/Odd
+
+let num;
+
+for(let i=1; i<=20; i++){
+  
+  if(i % 2 === 0){
+    console.log(`Even`, i)
+  }else{
+    console.log(`Odd`, i)
+  }
+}
+
+
